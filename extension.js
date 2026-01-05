8939986ea0869c9af77cb31c3e6c9f0be36eefcb
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2026-01-05 16:17:12 +0530

    Convert all files to ES6 modules for GNOME 49 compatibility
    GNOME 49 requires ES6 modules throughout the extension.
    The legacy 'imports' syntax no longer works.
    
    Changes:
    - extension.js: Convert to ES6 import/export syntax
    - calendarData.js: Use ES6 export and accept extension param
    - dateConverter.js: Use ES6 export and accept extension param
    - Use Extension base class from GNOME Shell
    - Pass extension reference for path access
    
    This fixes the 'import declarations may only appear at top level'
    error in GNOME 49.x

--- extension.js
+++ extension.js
@@ -1,15 +1,14 @@
-// Compatible imports for GNOME 42-47
-const GObject = imports.gi.GObject;
-const St = imports.gi.St;
-const Clutter = imports.gi.Clutter;
-const Main = imports.ui.main;
-const PanelMenu = imports.ui.panelMenu;
-const PopupMenu = imports.ui.popupMenu;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const CalendarData = Me.imports.calendarData;
-const DateConverter = Me.imports.dateConverter;
-const _ = ExtensionUtils.gettext;
+// ES6 imports for GNOME 45+
+import GObject from 'gi://GObject';
+import St from 'gi://St';
+import Clutter from 'gi://Clutter';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import {Extension, gettext as _} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+import {CalendarData} from './calendarData.js';
+import {DateConverter} from './dateConverter.js';
 
 // Helper function to convert Nepali numerals to Arabic numerals
 function nepaliToArabicNumeral(nepaliNum) {
@@ -30,9 +29,10 @@ function nepaliToArabicNumeral(nepaliNum) {
 
 const NepaliCalendarIndicator = GObject.registerClass(
     class NepaliCalendarIndicator extends PanelMenu.Button {
-        _init() {
+        _init(extension) {
             super._init(0.5, _('Nepali Calendar')); // 0.5 centers the button
 
+            this._extension = extension;
             this._signalConnections = [];
             this._gridSignalConnections = [];
 
@@ -62,8 +62,8 @@ const NepaliCalendarIndicator = GObject.registerClass(
 
             this.add_child(flagContainer);
 
-            this._calendarData = new CalendarData.CalendarData();
-            this._dateConverter = new DateConverter.DateConverter();
+            this._calendarData = new CalendarData(this._extension);
+            this._dateConverter = new DateConverter(this._extension);
             
             // Get current Nepali date and set it as default
             this._currentNepaliDate = this._dateConverter.getCurrentNepaliDate();
@@ -740,14 +740,10 @@ const NepaliCalendarIndicator = GObject.registerClass(
         }
     });
 
-class Extension {
-    constructor(uuid) {
-        this._uuid = uuid;
-    }
-
+export default class NepaliCalendarExtension extends Extension {
     enable() {
-        this._indicator = new NepaliCalendarIndicator();
-        Main.panel.addToStatusArea(this._uuid, this._indicator, 0, 'center');
+        this._indicator = new NepaliCalendarIndicator(this);
+        Main.panel.addToStatusArea(this.uuid, this._indicator, 0, 'center');
     }
 
     disable() {
@@ -757,9 +753,4 @@ class Extension {
             this._indicator = null;
         }
     }
-}
-
-function init(meta) {
-    ExtensionUtils.initTranslations();
-    return new Extension(meta.uuid);
 }
