ec8d4fc26cbf4fbe1eb62f8b19bdd910ff1a7a0e
Shudarsan Regmi <shudarsanregmi555@gmail.com>
2026-01-05 16:35:53 +0530

    Cleaned up unencessary files

--- logs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Monitoring logs for 'Simple Extension' and GNOME Shell errors..."
-echo "Press Ctrl+C to stop."
-echo "---------------------------------------------------------------"
-journalctl -f -o cat | grep --line-buffered -E "gnome-shell|Simple Extension|simple-extension"
